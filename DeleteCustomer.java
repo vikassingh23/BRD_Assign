295096e28760f125ff7089dd058a5b2ed00f9db3
Vikas Singh <31857454+vikassingh23@users.noreply.github.com>
2018-02-28 02:34:19 +0530

    Add files via upload

---
+++ DeleteCustomer.java
@@ -0,0 +1,78 @@
+package com.nucleus.makerchecker.controller.servlets;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class DeleteCustomer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.setContentType("application/vnd.ms-excel");
+				//HSSFWorkbook workbook = new HSSFWorkbook();
+				
+				
+				response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");		
+				response.setHeader("Content-Disposition", "attachment; filename=filename.xlsx");		
+				XSSFWorkbook workbook = new XSSFWorkbook();
+				XSSFSheet sheet = workbook.createSheet("sheet 1");
+				Map<String, Object[]> data = new TreeMap<String, Object[]>();
+		        data.put("1", new Object[]{ "ID", "NAME", "LASTNAME" });
+		        data.put("2", new Object[]{ 1, "Pankaj", "Kumar" });
+		        data.put("3", new Object[]{ 2, "Prakashni", "Yadav" });
+		        data.put("4", new Object[]{ 3, "Ayan", "Mondal" });
+		        data.put("5", new Object[]{ 4, "Virat", "kohli" });
+		 
+		        // Iterate over data and write to sheet
+		        Set<String> keyset = data.keySet();
+		        int rownum = 0;
+		        for (String key : keyset) {
+		            // this creates a new row in the sheet
+		            Row row = sheet.createRow(rownum++);
+		            Object[] objArr = data.get(key);
+		            int cellnum = 0;
+		            for (Object obj : objArr) {
+		                // this line creates a cell in the next column of that row
+		                Cell cell = row.createCell(cellnum++);
+		                if (obj instanceof String)
+		                    cell.setCellValue((String)obj);
+		                else if (obj instanceof Integer)
+		                    cell.setCellValue((Integer)obj);
+		            }
+		        }
+				
+				
+				// ...
+				// Now populate workbook the usual way.
+				// ...
+				workbook.write(response.getOutputStream()); // Write workbook to response.
+				workbook.close();
+				
+				
+				/*
+				String custcode = request.getParameter("customer_code");
+				System.out.println(custcode);
+				CustomerDao customerDao = new CustomerDao();
+				customerDao.deleteUser(custcode);
+				
+				response.sendRedirect("welcome.jsp");
+				*/
+			}
+	
+
+}
